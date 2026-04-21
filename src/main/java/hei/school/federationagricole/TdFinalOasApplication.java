e018d897eac19d1a895cc6fb78633fe3b30002c9
Thiannight <thianntatiana@gmail.com>
2026-04-21 15:14:43 +0300

    feat: spring boot architecture

---
+++ src/main/java/hei/school/federationagricole/TdFinalOasApplication.java
@@ -0,0 +1,13 @@
+package hei.school.federationagricole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdFinalOasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TdFinalOasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hei/school/federationagricole/TdFinalOasApplicationTests.java
@@ -0,0 +1,13 @@
+package hei.school.federationagricole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdFinalOasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
