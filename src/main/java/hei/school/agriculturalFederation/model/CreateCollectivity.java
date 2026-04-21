ec9d58c02961ba0071d53266880b5d7d163b620b
Thiannight <thianntatiana@gmail.com>
2026-04-21 20:57:57 +0300

    feat: missing model

---
+++ src/main/java/hei/school/agriculturalFederation/model/CreateCollectivity.java
@@ -0,0 +1,40 @@
+package hei.school.agriculturalFederation.model;
+
+import java.util.List;
+
+public class CreateCollectivity {
+    private String location;
+    private List<String> members;
+    private boolean federationApproval;
+    private CreateCollectivityStructure structure;
+
+    public CreateCollectivity() {}
+
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public List<String> getMembers() {
+        return members;
+    }
+    public void setMembers(List<String> members) {
+        this.members = members;
+    }
+
+    public boolean isFederationApproval() {
+        return federationApproval;
+    }
+    public void setFederationApproval(boolean federationApproval) {
+        this.federationApproval = federationApproval;
+    }
+
+    public CreateCollectivityStructure getStructure() {
+        return structure;
+    }
+    public void setStructure(CreateCollectivityStructure structure) {
+        this.structure = structure;
+    }
+}
