8937fb4684e09986353089013088584cd37b7f84
Thiannight <thianntatiana@gmail.com>
2026-04-22 18:11:45 +0300

    feat: MemberPayment and Transaction repositories

---
+++ src/main/java/hei/school/agriculturalFederation/repository/MemberPaymentRepository.java
@@ -0,0 +1,43 @@
+package hei.school.agriculturalFederation.repository;
+
+import hei.school.agriculturalFederation.datasource.DataSourceConfig;
+import hei.school.agriculturalFederation.model.*;
+import org.springframework.stereotype.Repository;
+
+import java.sql.*;
+
+@Repository
+public class MemberPaymentRepository {
+
+    private final DataSourceConfig dataSourceConfig;
+    private final FinancialAccountRepository financialAccountRepository;
+
+    public MemberPaymentRepository(DataSourceConfig dataSourceConfig,
+                                   FinancialAccountRepository financialAccountRepository) {
+        this.dataSourceConfig = dataSourceConfig;
+        this.financialAccountRepository = financialAccountRepository;
+    }
+
+    public MemberPayment save(String memberId, MemberPayment payment) {
+        String sql = """
+                INSERT INTO member_payment
+                  (id, member_id, amount, payment_mode, account_credited_id, creation_date)
+                VALUES (?, ?, ?, CAST(? AS payment_mode_enum), ?, ?)
+                """;
+        Connection conn = dataSourceConfig.getConnection();
+        try (PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, payment.getId());
+            ps.setString(2, memberId);
+            ps.setLong(3, payment.getAmount());
+            ps.setString(4, payment.getPaymentMode().name());
+            ps.setString(5, payment.getAccountCredited().getId());
+            ps.setDate(6, Date.valueOf(payment.getCreationDate()));
+            ps.executeUpdate();
+            return payment;
+        } catch (SQLException e) {
+            throw new RuntimeException("Error in save member_payment: " + e.getMessage(), e);
+        } finally {
+            dataSourceConfig.closeConnection(conn);
+        }
+    }
+}
