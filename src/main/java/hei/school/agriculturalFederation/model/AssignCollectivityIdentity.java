f8689d30e2bf43793a11670063fcacb3dcd146cc
Rel4w <hei.jonathan.4@gmail.com>
2026-04-22 09:05:39 +0300

    docs: update docs and feat: model

---
+++ src/main/java/hei/school/agriculturalFederation/model/AssignCollectivityIdentity.java
@@ -0,0 +1,4 @@
+package hei.school.agriculturalFederation.model;
+
+public class AssignCollectivityIdentity {
+}
--- src/main/java/hei/school/agriculturalFederation/model/Collectivity.java
+++ src/main/java/hei/school/agriculturalFederation/model/Collectivity.java
@@ -4,37 +4,29 @@ import java.util.List;
 
 public class Collectivity {
     private String id;
+    private String number;
+    private String name;
     private String location;
     private CollectivityStructure structure;
     private List<Member> members;
 
     public Collectivity() {}
 
-    public String getId() {
-        return id;
-    }
-    public void setId(String id) {
-        this.id = id;
-    }
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
 
-    public String getLocation() {
-        return location;
-    }
-    public void setLocation(String location) {
-        this.location = location;
-    }
+    public String getNumber() { return number; }
+    public void setNumber(String number) { this.number = number; }
 
-    public CollectivityStructure getStructure() {
-        return structure;
-    }
-    public void setStructure(CollectivityStructure structure) {
-        this.structure = structure;
-    }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 
-    public List<Member> getMembers() {
-        return members;
-    }
-    public void setMembers(List<Member> members) {
-        this.members = members;
-    }
+    public String getLocation() { return location; }
+    public void setLocation(String location) { this.location = location; }
+
+    public CollectivityStructure getStructure() { return structure; }
+    public void setStructure(CollectivityStructure structure) { this.structure = structure; }
+
+    public List<Member> getMembers() { return members; }
+    public void setMembers(List<Member> members) { this.members = members; }
 }
