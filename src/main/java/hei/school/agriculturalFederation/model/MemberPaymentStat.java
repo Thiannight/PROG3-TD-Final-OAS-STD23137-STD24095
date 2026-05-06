98948ecaa5b800e280b1e4d238416bd10ae777c2
Thiannight <thianntatiana@gmail.com>
2026-05-06 13:50:23 +0300

    fix: model not pushed

--- src/main/java/hei/school/agriculturalFederation/model/MemberPaymentStat.java
+++ src/main/java/hei/school/agriculturalFederation/model/MemberPaymentStat.java
@@ -1,4 +1,30 @@
 package hei.school.agriculturalFederation.model;
 
 public class MemberPaymentStat {
+    private Member member;
+    private double totalPaid;
+    private double totalUnpaid;
+
+    public MemberPaymentStat() {}
+
+    public Member getMember() {
+        return member;
+    }
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public double getTotalPaid() {
+        return totalPaid;
+    }
+    public void setTotalPaid(double totalPaid) {
+        this.totalPaid = totalPaid;
+    }
+
+    public double getTotalUnpaid() {
+        return totalUnpaid;
+    }
+    public void setTotalUnpaid(double totalUnpaid) {
+        this.totalUnpaid = totalUnpaid;
+    }
 }
