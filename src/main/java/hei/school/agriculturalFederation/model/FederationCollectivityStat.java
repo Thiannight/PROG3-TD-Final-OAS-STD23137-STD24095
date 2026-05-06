bbf277249aa43e24ab1aad5a05289d3826d8810e
Thiannight <thianntatiana@gmail.com>
2026-05-06 13:57:50 +0300

    feat: federation collectivy

--- src/main/java/hei/school/agriculturalFederation/model/FederationCollectivityStat.java
+++ src/main/java/hei/school/agriculturalFederation/model/FederationCollectivityStat.java
@@ -1,4 +1,38 @@
 package hei.school.agriculturalFederation.model;
 
 public class FederationCollectivityStat {
+    private String collectivityId;
+    private String collectivityName;
+    private double upToDateMembersPercentage;
+    private long newMembersCount;
+
+    public FederationCollectivityStat() {}
+
+    public String getCollectivityId() {
+        return collectivityId;
+    }
+    public void setCollectivityId(String collectivityId) {
+        this.collectivityId = collectivityId;
+    }
+
+    public String getCollectivityName() {
+        return collectivityName;
+    }
+    public void setCollectivityName(String collectivityName) {
+        this.collectivityName = collectivityName;
+    }
+
+    public double getUpToDateMembersPercentage() {
+        return upToDateMembersPercentage;
+    }
+    public void setUpToDateMembersPercentage(double upToDateMembersPercentage) {
+        this.upToDateMembersPercentage = upToDateMembersPercentage;
+    }
+
+    public long getNewMembersCount() {
+        return newMembersCount;
+    }
+    public void setNewMembersCount(long newMembersCount) {
+        this.newMembersCount = newMembersCount;
+    }
 }
